934237d04bd07a5dc0c0a54fc6aa406b23fb2b9a
Diogo Ribeiro <slysherz@gmail.com>
2019-12-16 00:01:15 +0000

    add new test

--- test/archive_test.py
+++ test/archive_test.py
@@ -13,11 +13,25 @@ class TestArchive(unittest.TestCase):
 
         self.assertEqual((link, tags), arc.get(1, ["link", "tags"]))
 
-        new_tag = ["new tag"]
+        new_tag = ["new tag1", "new tag2"]
         arc.update(1, {"tags": new_tag})
 
         self.assertEqual((new_tag,), arc.get(2, ["tags"]))
 
         # Broken links are not allowed
         self.assertRaises(Exception, arc.add, {"link": "broken link"})
+
+        # Complex update
+        arc.update(
+            2,
+            {
+                "tags": {"add": ["tag3", "tag4"], "sub": ["new tag1"]},
+                "name": ["new name"],
+            },
+        )
+
+        result = arc.get(3, ["name", "tags"])
+        self.assertEqual(result[0], "new name")
+
+        self.assertEqual(set(result[1]), {"new tag2", "tag4", "tag3"})
 
