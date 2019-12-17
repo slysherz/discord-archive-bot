9abbb74dffb644f5d0b4b18998c74fc29b443b06
Diogo Ribeiro <slysherz@gmail.com>
2019-12-17 17:01:02 +0000

    improve grammar tests

--- test/grammar_test.py
+++ test/grammar_test.py
@@ -1,10 +1,24 @@
 import unittest
 import grammar
+from grammar import Add, Sub
+
+g = grammar.build_grammar()
+
+
+def parse(command):
+    print("\nPARSING '%s':\n" % command)
+    parsed = g.parse(command)
+
+    print(parsed.pretty())
+
+    transformed = grammar.transform(parsed)
+    print(transformed)
+
+    return transformed
 
 
 class TestGrammar(unittest.TestCase):
     def test_grammar(self):
-        g = grammar.build_grammar()
 
         for s in [
             "!add link tags: author:slysherz score:1",
@@ -20,3 +34,39 @@ class TestGrammar(unittest.TestCase):
 
             transformed = grammar.transform(parsed)
             print(transformed)
+
+    def test_grammar2(self):
+        self.assertEqual(
+            parse("!add link tags: author:slysherz score:1"),
+            ["add", ["link", ("tags", ["author:slysherz", "score:1"])]],
+        )
+
+        self.assertEqual(
+            parse('!add link tags: [one, two] description: "looks really cool"'),
+            [
+                "add",
+                [
+                    "link",
+                    ("tags", [["one", "two"]]),
+                    ("description", ["looks really cool"]),
+                ],
+            ],
+        )
+
+        self.assertEqual(
+            parse("!update 123 tags: +[four, five, seven] -one"),
+            ["update", [123, ("tags", [Add(["four", "five", "seven"]), Sub("one")])],],
+        )
+
+        self.assertEqual(
+            parse("!find keyword [id, tags] tags: +author:? -score:?"),
+            [
+                "find",
+                [
+                    "keyword",
+                    ["id", "tags"],
+                    ("tags", [Add("author:?"), Sub("score:?")]),
+                ],
+            ],
+        )
+
